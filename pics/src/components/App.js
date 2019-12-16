8c3cf4984a48063a99726ae0d928fae188b35fed
EladRon <elad.ron.g@gmail.com>
2019-12-16 11:43:21 +0200

    integrating axios for web actions and unsplash for photos

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
     onSearchSubmit = term => {
-        console.log(term);
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term},
+            headers: {
+                Authorization: 'Client-ID f7b7460c741b55edc2d0eb7162079c6f959906d025969c049192f79cc8f14be8'
+
+            }
+        });
     }
 
     render() {
