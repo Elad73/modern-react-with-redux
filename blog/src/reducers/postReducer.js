d36072aa2ec43c9e91820fcc727f604abca006ce
EladRon <elad.ron.g@gmail.com>
2020-01-17 07:09:27 +0200

    Switch Statements in Reducers

--- blog/src/reducers/postReducer.js
+++ blog/src/reducers/postReducer.js
@@ -1,4 +1,9 @@
-export default () => {
-    return 123;
+export default (state = [], action) => {
+    switch(action.type) {
+        case 'FETCH_POSTS':
+            return action.payload;
+        deafult:
+            return state;
+    }
 };
 
