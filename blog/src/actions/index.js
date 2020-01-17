50281961f5895fbe04dd2fef46d118b28f4e8360
EladRon <elad.ron.g@gmail.com>
2020-01-17 10:25:42 +0200

    Fetching Singular Records

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -7,3 +7,9 @@ export const fetchPosts = () => {
         dispatch({ type: 'FETCH_POSTS', payload: response.data });
     };
 };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+};
