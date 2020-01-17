39cb1ffac789a3f794017235ef6484ce701c8284
EladRon <elad.ron.g@gmail.com>
2020-01-17 13:27:24 +0200

    Alternate Overfetching Solution

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -9,10 +9,16 @@ export const fetchPosts = () => {
     };
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceHolder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceHolder.get(`/users/${id}`);
+
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
