f1da3ac346aaa45472c9658748c1e03fa3c7b93f
EladRon <elad.ron.g@gmail.com>
2020-01-17 09:51:19 +0200

    Dispatching Correct Values

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,7 +3,7 @@ import jsonPlaceHolder from '../apis/jsonplaceholder';
 export const fetchPosts = () => {
     return async (dispatch, getState) => {
         const response = await jsonPlaceHolder.get('/posts');
-        
-        dispatch({ type: 'FETCH_POSTS', payload: response });
+
+        dispatch({ type: 'FETCH_POSTS', payload: response.data });
     };
 };
--- blog/src/reducers/postReducer.js
+++ blog/src/reducers/postReducer.js
@@ -1,8 +1,9 @@
 export default (state = [], action) => {
     switch(action.type) {
         case 'FETCH_POSTS':
+            //console.log("action.payload from reducer" + JSON.stringify(action.payload) + "/n/n/n/n");
             return action.payload;
-        deafult:
+        default:
             return state;
     }
 };
