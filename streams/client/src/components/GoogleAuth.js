1f698ff3ae2a9986d5361d5645d18b1144b698b2
EladRon <elad.ron.g@gmail.com>
2020-01-20 13:48:52 +0200

    Sending a User into the OAuth Flow

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -3,9 +3,11 @@ import React from 'react';
 class GoogleAuth extends React.Component {
 
     componentDidMount() {
+
+        console.log("Google client id3 -------------------------------------->" + JSON.stringify(process.env.REACT_APP_GOOGLE_CLIENT_ID))
         window.gapi.load('client:auth2', ()=>{
             window.gapi.client.init({
-                clientId: process.env.GOOGLE_CLIENT_ID,
+                clientId: process.env.REACT_APP_GOOGLE_CLIENT_ID,
                 scope: 'email'
             });
         });
