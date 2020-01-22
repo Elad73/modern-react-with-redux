5c9e2baf9b173e7dde21553d5a5c4f6f295842f0
EladRon <elad.ron.g@gmail.com>
2020-01-22 14:50:05 +0200

    Forms with Redux Form

--- streams/client/src/index.js
+++ streams/client/src/index.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
 import reducers from './reducers'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //wiring up the Redux Dev Tools to the store
 const store = createStore(
     reducers,
     composeEnhancers(applyMiddleware())
