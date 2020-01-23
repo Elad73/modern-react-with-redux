7bfbcd4e63f4138ae5ee5f2c09b584fe29d8b299
EladRon <elad.ron.g@gmail.com>
2020-01-23 14:07:50 +0200

    making reset for previous commit

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,5 +1,5 @@
 import streams from '../apis/streams';
-import { SIGN_IN, SIGN_OUT} from "./types";
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM} from "./types";
 
 export const signIn = (userId) => {
     return {
@@ -16,4 +16,6 @@ export const signOut = () => {
 
 export const createStream = formValues => async dispatch => {
       const response = await streams.post('/streams', formValues);
+
+      dispatch({ type: CREATE_STREAM, payload: response.data })
 };
