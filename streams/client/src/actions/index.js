c3bd4b2c86da0b097203d87bc6968ce147a9c656
EladRon <elad.ron.g@gmail.com>
2020-01-23 10:56:27 +0200

    commiting to continue working remotely...

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -15,5 +15,5 @@ export const signOut = () => {
 };
 
 export const createStream = formValues => async dispatch => {
-        streams.post('/streams', formValues);
-    };
+      const response = await streams.post('/streams', formValues);
+};
