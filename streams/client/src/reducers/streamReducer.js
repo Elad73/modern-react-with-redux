c6626e6130b7a50986f35f9f0c246327d3aec8b7
EladRon <elad.ron.g@gmail.com>
2020-01-24 10:01:33 +0200

    Fetching a List of All Streams

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -11,7 +11,7 @@ import {
 export default (state = {}, action) => {
     switch(action.type) {
         case FETCH_STREAMS:
-            return { ...state, ..._.mapKeys(action.paylaod, 'id')}
+            return { ...state, ..._.mapKeys(action.payload, 'id')}
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload}
         case CREATE_STREAM:
