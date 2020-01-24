8629ee4ffff14c5d19843430ef50de770393920e
EladRon <elad.ron.g@gmail.com>
2020-01-24 14:58:05 +0200

    Wildcard Navigation

--- streams/client/src/components/Streams/StreamList.js
+++ streams/client/src/components/Streams/StreamList.js
@@ -13,12 +13,8 @@ class StreamList extends React.Component{
         if (stream.userId === this.props.currentUserId) {
             return (
                 <div className="right floated content">
-                    <button className="ui button primary">
-                        Edit
-                    </button>
-                    <button className="ui button negative">
-                        Delete
-                    </button>
+                    <Link to={`/streams/edit/${stream.id}`} className="ui button primary">Edit</Link>
+                    <button className="ui button negative">Delete</button>
                 </div>
             );
         }
