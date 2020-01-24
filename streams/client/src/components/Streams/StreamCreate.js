e8c74c927ab338445bfab5797657b576ef1bcd91
EladRon <elad.ron.g@gmail.com>
2020-01-24 09:39:29 +0200

    Merging Lists of Records

--- streams/client/src/components/Streams/StreamCreate.js
+++ streams/client/src/components/Streams/StreamCreate.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { createStream } from '../../actions';
-import { createStore } from 'redux';
 
 class StreamCreate extends React.Component {
     renderError({ error, touched }) {
