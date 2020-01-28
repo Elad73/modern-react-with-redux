4bb38abc427a9c789f49e16b3c3ecbf3e627dd63
EladRon <elad.ron.g@gmail.com>
2020-01-28 14:17:10 +0200

    Implementing and rendering the LanguageStore

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 import LanguageSelector from '../components/LanguageSelector';
 
 class App extends React.Component {
-    state = { language: 'english'};
-    
-    onLanguageChange = language => {
-        this.setState({ language });
-    }
-
     render() {
         return (
             <div className="ui container">
-                <LanguageSelector onLanguageChange={this.onLanguageChange} />
-                <ColorContext.Provider value="green">
-                    <LanguageContext.Provider value={this.state.language}>
-                        <UserCreate />
-                    </LanguageContext.Provider>
-                </ColorContext.Provider>
+                <LanguageStore>
+                    <LanguageSelector />
+
+                    <ColorContext.Provider value="green">
+                            <UserCreate />
+                    </ColorContext.Provider>
+                </LanguageStore>
             </div>
         );
     }
--- translate/src/contexts/LanguageContext.js
+++ translate/src/contexts/LanguageContext.js
@@ -1,3 +1,22 @@
 import React from 'react';
 
-export default React.createContext('english'); //initializing the context with default value
+ const Context = React.createContext('english'); //initializing the context with default value
+
+ export class LanguageStore extends React.Component {
+    state = { language: 'english'};
+
+    onLanguageChange = language => {
+        this.setState({ language });
+    };
+
+    render() {
+        console.log(this.onLanguageChange);
+        return (
+            <Context.Provider value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
+                {this.props.children}
+            </Context.Provider>
+        );
+    }
+ }
+
+ export default Context;
