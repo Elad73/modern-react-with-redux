6b60a3cc5de88f3e5c25d76d31d10965556a8d54
EladRon <elad.ron.g@gmail.com>
2020-01-28 18:16:38 +0200

    Updates with useEffect

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,28 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-    state = { resources: [] };
+const ResourceList = ({ resource }) => {
+    const [resources, setResources] = useState([]);
 
-    async fetchData() {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+    const fetchResource = async (resource) => {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
 
-        this.setState({ resources: response.data});
+        setResources(response.data);
     }
 
-    componentDidMount() {
-       this.fetchData();
-    }
+    useEffect(() => {
+        fetchResource(resource);
+    }, [resource]);
 
-    componentDidUpdate(prevProps) {
-        if(prevProps.resource !== this.props.resource) {
-            this.fetchData();
-        }
-    }
-
-    render() {
-        return <div>{this.state.resources.length}</div>;
-    }
+    return <div>{resources.length}</div>;
 }
 
 export default ResourceList;
