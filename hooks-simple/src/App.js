0582c489be24a2545cf133f5501872c2ab30d92b
EladRon <elad.ron.g@gmail.com>
2020-01-28 15:05:26 +0200

    The useState Hook

--- hooks-simple/src/App.js
+++ hooks-simple/src/App.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
 
-class App extends React.Component {
-    state = { resource: 'posts'};
 
-    render() {
-        return (
+const App = () => {
+    const [resource, setResource] = useState('posts');
+
+    return (
+        <div>
             <div>
-                <div>
-                        <button onClick={()=> this.setState({ resource: 'posts'})}>Posts</button>
-                        <button onClick={()=> this.setState({ resource: 'todos'})}>Todos</button>
-                </div>
-                {this.state.resource}
+                <button onClick={()=> setResource('posts') }>Posts</button>
+                <button onClick={()=> setResource('todos')}>Todos</button>
             </div>
-        );
-    }
+            {resource}
+        </div>
+    );
 }
 
 export default App;
