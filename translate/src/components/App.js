44600956845e0955fb000848d64e62dea68755ef
EladRon <elad.ron.g@gmail.com>
2020-01-28 12:01:09 +0200

    adding all of the components

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
 class App extends React.Component {
+    state = { language: 'english'};
+    
+    onLanguageChange = language => {
+        this.setState({ language });
+    }
+
     render() {
-        return <div className="ui container">App</div>;
+        return (
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us" onClick={() => this.onLanguageChange('english')}  />
+                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+                </div>
+                <UserCreate />
+            </div>
+        );
     }
 }
 
