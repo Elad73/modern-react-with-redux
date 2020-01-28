92a57300b40ce98584e3328335e1282f04b3bbbc
EladRon <elad.ron.g@gmail.com>
2020-01-28 17:59:14 +0200

    ComponentDidMOunt vs ComponentDidUpdate - be carefull from endless loop

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,8 +1,27 @@
 import React from 'react';
+import axios from 'axios';
 
 class ResourceList extends React.Component {
+    state = { resources: [] };
+
+    async fetchData() {
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+
+        this.setState({ resources: response.data});
+    }
+
+    componentDidMount() {
+       this.fetchData();
+    }
+
+    componentDidUpdate(prevProps) {
+        if(prevProps.resource !== this.props.resource) {
+            this.fetchData();
+        }
+    }
+
     render() {
-    return <div>{this.props.resource}</div>;
+        return <div>{this.state.resources.length}</div>;
     }
 }
 
