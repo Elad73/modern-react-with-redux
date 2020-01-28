ce3266560b5bcd81190365949b26eceab78a3913
EladRon <elad.ron.g@gmail.com>
2020-01-28 19:10:24 +0200

    Quick Gotcha with useEffect and rendering the list

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -4,17 +4,20 @@ import axios from 'axios';
 const ResourceList = ({ resource }) => {
     const [resources, setResources] = useState([]);
 
-    const fetchResource = async (resource) => {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+    useEffect(
+        () => {
+            (async resource => {
+                const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+                
+                setResources(response.data);
+            })(resource);
+        }, 
+        [resource]
+    );
 
-        setResources(response.data);
-    }
-
-    useEffect(() => {
-        fetchResource(resource);
-    }, [resource]);
-
-    return <div>{resources.length}</div>;
+    return (
+    <ul>{resources.map(record => <li key={record.id}>{record.title}</li>)}</ul>
+    );
 }
 
 export default ResourceList;
